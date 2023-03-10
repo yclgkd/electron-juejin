ff4a4ca274e6abe8dd0c27347e6b1ae81002059a
brian <chunlai0928@gmail.com>
2023-03-10 12:59:30 +0800

    first commit

---
+++ src/main/mainEntry.ts
@@ -0,0 +1,27 @@
+import { app, BrowserWindow } from "electron";
+import { CustomScheme } from "./CustomScheme";
+
+process.env.ELECTRON_DISABLE_SECURITY_WARNINGS = "true";
+let mainWindow: BrowserWindow;
+
+app.whenReady().then(() => {
+  let config = {
+    webPreferences: {
+      nodeIntegration: true,
+      webSecurity: false,
+      allowRunningInsecureContent: true,
+      contextIsolation: false,
+      webviewTag: true,
+      spellcheck: false,
+      disableHtmlFullscreenWindowResize: true,
+    },
+  };
+  mainWindow = new BrowserWindow(config);
+  if (process.argv[2]) {
+    mainWindow.loadURL(process.argv[2]);
+    mainWindow.webContents.openDevTools({ mode: "undocked" });
+  } else {
+    CustomScheme.registerScheme();
+    mainWindow.loadURL(`app://index.html`);
+  }
+});
